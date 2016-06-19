9118be3b3ebac99b01a41a6faefda465f922305f
Alonso S <alonsosalcido13@gmail.com>
2016-06-18 18:18:52 -0600

    Component component, component container modified 5th video

--- client/main.js
+++ client/main.js
@@ -2,13 +2,48 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Meteor } from 'meteor/meteor'
 
+class Comment extends React.Component{
+	constructor(){
+		super()
+	}
+
+	render(){
+		return(<h4>{this.props.comment}</h4>)
+	}
+}
+
 class App extends React.Component{
 	constructor(){
 		super()
 	}
 
 	render(){
-		return(<h1>Hola mundo, desde React JS</h1>)
+		let comments = [
+			{
+				_id: new Date(),
+				comment: 'Hola meteor '
+
+			},
+			{
+				_id: new Date()+1,
+				comment: 'Hola meteor '
+
+			},
+			{
+				_id: new Date()+2,
+				comment: 'Hola meteor '
+
+			}
+		]
+
+		let renderComments = comments.map((comment)=>{
+			return(<Comment key={comment._id} comment={comment.comment}/>)
+		})
+		return(
+			<div>
+				{renderComments}
+			</div>
+		)
 	}
 }
 
